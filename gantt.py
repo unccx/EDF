98ad57a89c0475aca38080f639722300c09af24c
arc <2336429253@qq.com>
2023-05-24 18:22:03 +0800

    更新生成gantt图的注释

--- gantt.py
+++ gantt.py
@@ -13,11 +13,12 @@ def plot_gantt(scheduler):
     # Setting graph attribute
     ax.grid(True)
 
+    # 绘制条形图，并将任务序号写在矩形中间
     for i, processor in enumerate(scheduler.processors):
+        # 绘制代表processor这一行的running history
         for record in processor.history:
-            task_id = record[0]
-            task_instance_id = record[1]
-            color = 'C' + str(task_id % 10)
+            task_id, task_instance_id = record[0:2]
+            color = 'C' + str(task_id % 10) 
             ax.broken_barh([record[2:4]], (i*10, 4), facecolors=color)
             ax.annotate(f"t{task_id},{task_instance_id}", 
                         xy=(record[2]+record[3]/2, i*10+2), ha='center', va='center')
