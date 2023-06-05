b2a733de473604abefb7046751465c1ba48dce5d
arc <2336429253@qq.com>
2023-06-05 21:02:57 +0800

    添加数据生成器

---
+++ data_generater.py
@@ -0,0 +1,92 @@
+import scheduler as sc
+import numpy as np
+import itertools
+
+np.random.seed(42) 
+
+class DataGenerator(object):
+    def __init__(self):
+        self.processors = []
+        self.tasks: np.ndarray
+        self.hyperedges = []
+
+    def generate_platform(self, processors_number):
+        """随机生成一组速度不同的异构处理器平台"""
+        for i in range(0, processors_number):
+            processor = sc.Processor(id=f"P{i}", speed=np.random.randint(1, 10))
+            self.processors.append(processor)
+        
+        return self.processors
+
+    def generate_task(self, number_of_tasks):
+        # 生成 number_of_tasks 个二元组
+        binaries = np.random.randint(1, 101, size=(number_of_tasks, 2))
+
+        # 重复最后一列加到最后，即三元组中的deadline == period
+        triplets = np.hstack((binaries, binaries[:, -1].reshape(-1, 1)))
+
+        # # 筛选出第二个元素不大于第三个元素的三元组
+        # mask = triplets[:, 1] <= triplets[:, 2]
+        # triplets = triplets[mask]
+
+        self.tasks = triplets
+
+        return triplets
+
+    def judge_feasibility(self, task_id_set: list):
+        # 任务集为空，不需要调度
+        if not task_id_set:
+            print("task_id_set is empty and does not need to be scheduled")
+            return
+
+        # 判断处理器平台是否为空，为空则需要生成处理器平台
+        if not self.processors:
+            print("processors is empty, need to generate processor platform")
+            return 
+        
+        scheduler = sc.Scheduler(self.processors)
+
+        # 把 task_id_set 中的 task_id 对应的 task 添加到 scheduler 中
+        for task_id in task_id_set:
+            e, d, T = self.tasks[task_id, :]
+            task = sc.Task(task_id, arrival_timepoint=0, execution_time=e, deadline=d, period=T)
+            scheduler.add_task(task)
+
+        # 模拟调度过程判断任务集是否可调度
+        feasible: bool = scheduler.run()
+        return feasible
+
+    def generate_hyperedge(self):
+        """为节点集合充分的生成超边
+        保存在self.hyperedges中
+        """
+        number_of_tasks = self.tasks.shape[0]
+        self.search_hyperedge(list(range(number_of_tasks)))
+        return self.hyperedges
+
+
+    def search_hyperedge(self, task_id_set):
+        """从一组任务节点中递归地找出所有的超边"""
+        # 判断 task_id_set 是否为空
+        if not task_id_set:
+            return
+
+        # 判断 task_set 在 processors 这个平台上是否可调度
+        if self.judge_feasibility(task_id_set):
+            self.hyperedges.append(task_id_set)
+
+        # task_id_set 在 processors 上不可调度，判断 task_id_set 的子集是否可调度
+        combin = itertools.combinations(task_id_set, task_id_set.len()-1)
+        for i, subset in enumerate(combin):
+            print(f"{i}: {subset}")
+            self.search_hyperedge(subset)
+
+# dg = DataGenerator()
+# task_set = dg.generate_task(15)
+# platform = dg.generate_platform(10)
+# feasible = dg.judge_feasibility([0, 1, 2, 3, 4])
+
+# if feasible:
+#     print("The tasks are feasible")
+# else:
+#     print("The tasks are not feasible")
--- main.py
+++ main.py
@@ -1,41 +1,49 @@
 import scheduler as sc
 import gantt
+import data_generater as data
 
 def main():
-    processors = [
-        sc.Processor(id='A', speed=1),
-        # sc.Processor(id='B', speed=1),
-        # sc.Processor(id='C', speed=1),
-        # sc.Processor(id='D', speed=1),
-    ]
+    dg = data.DataGenerator()
+    task_set = dg.generate_task(15)
+    platform = dg.generate_platform(10)
+    dg.generate_hyperedge()
 
-    # # 将处理器按照speed进行降序排序
-    # processors.sort(key=lambda processor : processor.speed, reverse=True)
+    print(dg.hyperedges)
 
-    tasks = [
-        sc.Task(1, arrival_timepoint=0, execution_time=25, deadline=50, period=50),
-        sc.Task(2, arrival_timepoint=0, execution_time=30, deadline=75, period=75),
-        # sc.Task(3, arrival_timepoint=0, execution_time=20, deadline=11, period=11),
-        # sc.Task(4, arrival_timepoint=0, execution_time=6, deadline=10, period=10),
-        # sc.Task(5, arrival_timepoint=0, execution_time=6, deadline=10, period=10),
-    ]
+    # processors = [
+    #     sc.Processor(id='A', speed=1),
+    #     # sc.Processor(id='B', speed=1),
+    #     # sc.Processor(id='C', speed=1),
+    #     # sc.Processor(id='D', speed=1),
+    # ]
 
-    scheduler = sc.Scheduler(processors)
-    for task in tasks:
-        scheduler.add_task(task)
+    # # # 将处理器按照speed进行降序排序
+    # # processors.sort(key=lambda processor : processor.speed, reverse=True)
 
-    print(f"lcm:{scheduler.lcm_period}")
+    # tasks = [
+    #     sc.Task(1, arrival_timepoint=0, execution_time=25, deadline=50, period=50),
+    #     sc.Task(2, arrival_timepoint=0, execution_time=30, deadline=75, period=75),
+    #     # sc.Task(3, arrival_timepoint=0, execution_time=20, deadline=11, period=11),
+    #     # sc.Task(4, arrival_timepoint=0, execution_time=6, deadline=10, period=10),
+    #     # sc.Task(5, arrival_timepoint=0, execution_time=6, deadline=10, period=10),
+    # ]
 
-    feasible = scheduler.run()
-    if feasible:
-        print("The tasks are feasible")
-    else:
-        print("The tasks are not feasible")
+    # scheduler = sc.Scheduler(processors)
+    # for task in tasks:
+    #     scheduler.add_task(task)
 
-    for processor in processors:
-        print(f"{processor.id}:{processor.history}")
+    # print(f"lcm:{scheduler.lcm_period}")
 
-    gantt.plot_gantt(scheduler)
+    # feasible = scheduler.run()
+    # if feasible:
+    #     print("The tasks are feasible")
+    # else:
+    #     print("The tasks are not feasible")
+
+    # for processor in processors:
+    #     print(f"{processor.id}:{processor.history}")
+
+    # gantt.plot_gantt(scheduler)
 
 if __name__ == "__main__":
     main()
